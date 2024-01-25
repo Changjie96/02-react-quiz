3a9814c5caabd74d0e54a2a3c282716a598de33c
ChangJie Tan <tan7cj@gmail.com>
2024-01-25 15:33:57 +0800

    fix apos

--- app/components/StartScreen.js
+++ app/components/StartScreen.js
@@ -3,7 +3,7 @@ function StartScreen({ numQuestions, dispatch }) {
     <div className="start">
       <h2>Welcome to The React Quiz</h2>
       <h3>{numQuestions} question to test your React mastery</h3>
-      <button className="btn btn-ui" onClick={() => dispatch({ type: 'start' })}>Let's start</button>
+      <button className="btn btn-ui" onClick={() => dispatch({ type: 'start' })}>Let&apos;s start</button>
     </div>
   )
 }
